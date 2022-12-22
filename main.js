8c6d739b45297cee2dbbd719904c426cf6c48b82
niranjan dorage <108044574+Niranjan-Dorage@users.noreply.github.com>
2022-12-22 21:57:33 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,350 @@
+let a = [
+    "Q:What goes up and down but does not move? ",
+
+    "Q:Where should a 500 pound alien go? ",
+
+    "Q:What did one toilet say to the other?",
+
+    "Q:Why did the picture go to jail?",
+
+    "Q:What did one wall say to the other wall?",
+
+    "Q:What did the paper say to the pencil?",
+
+    "Q:What do you call a boy named Lee that no one talks to?",
+
+    "Q:What gets wetter the more it dries? ",
+
+    "Q:Why do bicycles fall over?",
+
+    "Q:Why do dragons sleep during the day?",
+
+    "Q:What did Cinderella say when her photos did not show up",
+
+    "Q:Why was the broom late?",
+
+    "Q:What part of the car is the laziest?",
+
+    "Q:What did the stamp say to the envelope?",
+
+    "Q:What is blue and goes ding dong?   ",
+
+    "Q:Were you long in the hospital?  ",
+
+    "Q:Why couldn't the pirate play cards?  ",
+
+    "Q:What did the laundryman say to the impatient customer?  ",
+
+    "Q:What's the difference between a TV and a newspaper?",
+
+    "Q:What did one elevator say to the other elevator?  ",
+
+    "Q:Why was the belt arrested?  ",
+
+    "Q:Why was everyone so tired on April 1st?  ",
+
+    "Q:Which hand is it better to write with?  ",
+
+    "Q:Why can't your nose be 12 inches long?   ",
+
+    "Q:What makes the calendar seem so popular?   ",
+
+    "Q:Why did Mickey Mouse take a trip into space? ",
+
+    "Q:What is green and has yellow wheels? ",
+
+    "Q:What is it that even the most careful person overlooks? ",
+
+    "Q:Did you hear about the robbery last night?   ",
+
+    "Q:Why do you go to bed every night? ",
+
+    "Q:Why did Billy go out with a prune?   ",
+
+    "Q:Why do eskimos do their laundry in Tide?  ",
+
+    "Q:How do you cure a headache?  ",
+
+    "Q:What has four wheels and flies? ",
+
+    "Q:What kind of car does Mickey Mouse's wife drive?  ",
+
+    "Q:Why don't traffic lights ever go swimming?  ",
+
+    "Q:Why did the robber take a bath before he stole from the bank? "]
+
+let ans = [
+    "A:Stairs",
+    "A: On a diet",
+    "A: You look a bit flushed.",
+    "A: Because it was framed.",
+    "A: I'll meet you at the corner.",
+    "A: Write on!",
+    "A: Lonely",
+    "A towel.",
+    "A: Because they are two - tired!",
+    "A: So they can fight knights!",
+    "A: Someday my prints will come!",
+    "A: It over swept!",
+    "A: The wheels, because they are always tired!",
+    "A: Stick with me and we will go places!",
+    "A: An Avon lady at the North Pole!",
+    "A: No, I was the same size I am now!",
+    "A: Because he was sitting on the deck!",
+    "A: Keep your shirt on!",
+    "A: Ever tried swatting a fly with a TV ?",
+    "A: I think I'm coming down with something!",
+    "A: Because it held up some pants!",
+    "A: They had just finished a March of 31 days.",
+    "A: Neither, it's best to write with a pen!",
+    "A: Because then it would be a foot!",
+    "A: Because it has a lot of dates!",
+    "A: He wanted to find Pluto!",
+    "A: Grass…..I lied about the wheels!",
+    "A: Her nose!",
+    "A: Two clothes pins held up a pair of pants!",
+    "A: Because the bed won't come to you!",
+    "A: Because he couldn't find a date!",
+    "A: Because it's too cold out-tide!",
+    "A: Put your head through a window and the pane will just disappear!",
+    "A: A garbage truck!",
+    "A: A minnie van!",
+    "A: Because they take too long to change! ",
+    "A: He wanted to make a clean get away!"
+
+
+]
+
+let one = [
+
+    "My wife told me to stop impersonating a flamingo. I had to put my foot down",
+
+    "I went to buy some camo pants but couldn’t find any",
+
+    "I failed math so many times at school, I can’t even count",
+
+    "I used to have a handle on life, but then it broke",
+
+    "I was wondering why the frisbee kept getting bigger and bigger, but then it hit me",
+
+    "I heard there were a bunch of break-ins over at the car park. That is wrong on so many levels",
+
+    "I want to die peacefully in my sleep, like my grandfather… Not screaming and yelling like the passengers in his car",
+
+    "When life gives you melons, you might be dyslexic",
+
+    "Don’t you hate it when someone answers their own questions? I do",
+
+    "It takes a lot of balls to golf the way I do",
+
+    "I told him to be himself; that was pretty mean, I guess",
+
+    "I know they say that money talks, but all mine says is ‘Goodbye.",
+
+    "My father has schizophrenia, but he’s good people",
+
+    "The problem with kleptomaniacs is that they always take things literally",
+
+    "I can’t believe I got fired from the calendar factory. All I did was take a day off",
+
+    "Most people are shocked when they find out how bad I am as an electrician",
+
+    "Never trust atoms; they make up everything",
+
+    "My wife just found out I replaced our bed with a trampoline. She hit the ceiling",
+
+    "I was addicted to the hokey pokey, but then I turned myself around",
+
+    "I used to think I was indecisive. But now I’m not so sure",
+
+    "Russian dolls are so full of themselves",
+
+    "The easiest time to add insult to injury is when you’re signing someone’s cast",
+
+    "Light travels faster than sound, which is the reason that some people appear bright before you hear them speak",
+
+    "My therapist says I have a preoccupation for revenge. We’ll see about that",
+
+    "A termite walks into the bar and asks, ‘Is the bar tender here?",
+
+    "A told my girlfriend she drew her eyebrows too high. She seemed surprised",
+
+    "People who use selfie sticks really need to have a good, long look at themselves",
+
+    "Two fish are in a tank. One says, ‘How do you drive this thing?",
+
+    "I always take life with a grain of salt. And a slice of lemon. And a shot of tequila",
+
+    "Just burned 2,000 calories. That’s the last time I leave brownies in the oven while I nap",
+
+    "Build a man a fire and he’ll be warm for a day. Set a man on fire and he’ll be warm for the rest of his life",
+
+    "I don’t suffer from insanity—I enjoy every minute of it",
+
+    "The last thing I want to do is hurt you; but it’s still on the list",
+
+    "The problem isn’t that obesity runs in your family. It’s that no one runs in your family",
+
+    "Today a man knocked on my door and asked for a small donation toward the local swimming pool. I gave him a glass of water",
+
+    "Build a man a fire and he’ll be warm for a day. Set a man on fire and he’ll be warm for the rest of his life",
+
+    "I don’t suffer from insanity—I enjoy every minute of it",
+
+    "The last thing I want to do is hurt you; but it’s still on the list",
+
+    "The problem isn’t that obesity runs in your family. It’s that no one runs in your family",
+
+    "Today a man knocked on my door and asked for a small donation toward the local swimming pool. I gave him a glass of water",
+
+    "The future, the present, and the past walk into a bar. Things got a little tense",
+
+    "Before you criticize someone, walk a mile in their shoes. That way, when you do criticize them, you’re a mile away and you have their shoes",
+
+    "Last night my girlfriend was complaining that I never listen to her… or something like that",
+
+    "Maybe if we start telling people their brain is an app, they’ll want to use it",
+
+    "If a parsley farmer gets sued, can they garnish his wages",
+
+    "I got a new pair of gloves today, but they’re both ‘lefts,’ which on the one hand is great, but on the other, it’s just not right",
+
+    "I didn’t think orthopedic shoes would help, but I stand corrected",
+
+    "I was riding a donkey the other day when someone threw a rock at me and I fell off. I guess I was stoned off my ass",
+
+    "People who take care of chickens are literally chicken tenders",
+
+    "It was an emotional wedding, Even the cake was in tiers",
+
+    "I just got kicked out of a secret cooking society. I spilled the beans",
+
+    "What’s a frog’s favorite type of shoes? Open toad sandals",
+
+    "Blunt pencils are really pointless",
+
+    "6:30 is the best time on a clock, hands down",
+
+    "Two wifi engineers got married. The reception was fantastic",
+
+    "Just got fired from my job as a set designer. I left without making a scene",
+
+    "What’s the difference between ignorance and apathy? I don’t know and I don’t care",
+
+    "One of the cows didn’t produce milk today. It was an udder failure",
+
+    "Adam & Eve were the first ones to ignore the Apple terms and conditions",
+
+    "Refusing to go to the gym is a form of resistance training",
+
+    "If attacked by a mob of clowns, go for the juggler",
+
+    "The man who invented Velcro has died. RIP",
+
+    "Despite the high cost of living, it remains popular",
+
+    "A dung beetle walks into a bar and asks, ‘Is this stool taken?",
+
+    "I can tell when people are being judgmental just by looking at them",
+
+    "The rotation of Earth really makes my day",
+
+    "Well, to be Frank with you, I’d have to change my name",
+
+    "My friend was explaining electricity to me, but I was like, ‘Watt?’"
+]
+function alertt() {
+    alert("sorry we don't have more jokes!!");
+}
+function randomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
+// document.write("Random Number between 1 and 100: ") 
+
+// // Function call
+// document.write( randomNumber(1, 100) ); 
+
+let temp = 0;
+let j;
+
+function questionanswer() {
+    let i = randomNumber(0, 36);
+
+    if (temp == 0) {
+        j = i;
+    }
+    // let inputText = document.querySelector('.inp').value;
+    let list = document.querySelector('.container ul');
+
+    // buttonn.addEventListener('click', (e) => {
+    //     if (inputText.value != "") {
+    //         e.preventDefault();
+    if (temp == 0) {
+        document.getElementById('btn2').innerHTML = "Silly Answer?"
+        //want answer?
+        console.log('hi');
+        let lii = document.createElement('li');
+        lii.innerHTML = a[i];
+        list.appendChild(lii);
+        const msp = document.createElement('span');
+        msp.innerHTML = 'X';
+        lii.appendChild(msp);
+        const close = document.querySelectorAll('span');
+        for (let i = 0; i < close.length; i++) {
+            close[i].addEventListener('click', () => {
+                close[i].parentElement.style.display = "none";
+            })
+
+        }
+        temp++;
+    }
+    else {
+        document.getElementById('btn2').innerHTML = "Question?";
+
+        //question?
+        console.log('hi');
+        let lii = document.createElement('li');
+        lii.innerHTML = ans[j];
+        list.appendChild(lii);
+        const msp = document.createElement('span');
+        msp.innerHTML = 'X';
+        lii.appendChild(msp);
+        const close = document.querySelectorAll('span');
+        for (let i = 0; i < close.length; i++) {
+            close[i].addEventListener('click', () => {
+                close[i].parentElement.style.display = "none";
+            })
+
+        }
+        temp = 0;
+    }
+
+
+
+    //     }
+    // })
+}
+function display() {
+    document.getElementById('li1').style.display = 'none';
+
+}
+function jokes() {
+    let list = document.querySelector('.container ul');
+    let i = randomNumber(0, 36);
+    console.log('hi');
+    let lii = document.createElement('li');
+    lii.innerHTML = one[i];
+    list.appendChild(lii);
+    const msp = document.createElement('span');
+    msp.innerHTML = 'X';
+    lii.appendChild(msp);
+    const close = document.querySelectorAll('span');
+    for (let i = 0; i < close.length; i++) {
+        close[i].addEventListener('click', () => {
+            close[i].parentElement.style.display = "none";
+        })
+
+    }
+
+}
